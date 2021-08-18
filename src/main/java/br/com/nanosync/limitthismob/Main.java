ebaba34f4787b025eeae0cb44032e39b51164449
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-08-18 20:12:43 -0300

    🔥 feat turn on feats

--- src/main/java/br/com/nanosync/limitthismob/Main.java
+++ src/main/java/br/com/nanosync/limitthismob/Main.java
@@ -2,14 +2,23 @@ package br.com.nanosync.limitthismob;
 
 import br.com.nanosync.limitthismob.commands.LimitMobPerChunkCommand;
 import br.com.nanosync.limitthismob.config.MobsConfig;
+import br.com.nanosync.limitthismob.events.LimitMobChunkEvent;
+import lombok.SneakyThrows;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    @SneakyThrows
     @Override
     public void onEnable() {
+        Class.forName("org.h2.Driver");
         saveDefaultConfig();
         new MobsConfig().createConfigs();
+        PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new LimitMobChunkEvent(), this);
+
         getCommand("teste").setExecutor(new LimitMobPerChunkCommand());
 
     }
