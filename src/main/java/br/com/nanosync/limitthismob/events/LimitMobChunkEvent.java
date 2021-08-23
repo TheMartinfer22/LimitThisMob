3f0bb485d21fe969946976de4622f4c6bcea872a
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-08-22 21:59:31 -0300

    🔥 feat to make read a static variable and improve performance

--- src/main/java/br/com/nanosync/limitthismob/events/LimitMobChunkEvent.java
+++ src/main/java/br/com/nanosync/limitthismob/events/LimitMobChunkEvent.java
@@ -1,21 +1,20 @@
 package br.com.nanosync.limitthismob.events;
 
-import br.com.nanosync.limitthismob.database.DatabaseAPI;
-import org.bukkit.Bukkit;
+import br.com.nanosync.limitthismob.memories.api.MemoriesAPI;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
-
 import java.util.Arrays;
 
 public class LimitMobChunkEvent implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void limit(EntitySpawnEvent event){
-        DatabaseAPI databaseAPI = new DatabaseAPI();
-        databaseAPI.listWprlds().forEach(entryWorld -> {
+
+        MemoriesAPI.getMemoriesWorldsAPI().forEach(entryWorld -> {
             if (event.getLocation().getWorld().getName().equals(entryWorld)){
-                databaseAPI.listMobChunk().forEach((mob, quantity) -> {
+                MemoriesAPI.getMemoriesMobsAPI().forEach((mob, quantity) -> {
                     if  (event.getEntity().getName().equals(mob)){
                         int quantidade = (int) Arrays.stream(event.getLocation().getChunk().getEntities()).filter(e -> e.getName().equals(mob)).count();
                         if (quantidade >= quantity){
